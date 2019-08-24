0bb0e6815a8a9c092554fe99bc8470694c2464d8
Henry Conklin <henrywconklin@gmail.com>
2019-08-24 19:09:13 -0400

    Fix description in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='plsnocrash',
-    version='0.1.0',
+    version='0.1.1',
     packages=['plsnocrash'],
     url='https://github.com/HenryWConklin/plsnocrash',
     license='',
     author='Henry Conklin',
     author_email='henrywconklin@gmail.com',
     description='Ever had your code crash after 10 hours of heavy computation because of a dumb mistake?',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     test_suite="test"
 )
