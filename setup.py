e095813893b2de9b78032f7db573138b3b12d042
Henry Conklin <henrywconklin@gmail.com>
2019-09-15 14:12:53 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='plsnocrash',
-    version='0.1.2',
+    version='0.1.3',
     packages=['plsnocrash'],
     url='https://github.com/HenryWConklin/plsnocrash',
     license='',
